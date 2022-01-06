2450294eeae880ffa6c2e53f5314605d40cffc79
Isaac King <idrkings@gmail.com>
2022-01-05 23:50:55 -0800

    Initial setup w/ Vanta

---
+++ pages/_document.js
@@ -0,0 +1,23 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
+
+class MyDocument extends Document {
+
+  render() {
+    return (
+      <Html>
+        <Head>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r121/three.min.js"></script>
+        </Head>
+        <body>
+        
+          
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
