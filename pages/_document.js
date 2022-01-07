eb58c030c3ca505cf4d2046152a0ef5fed219324
Idrking <idrkings@gmail.com>
2022-01-06 22:05:07 -0800

    cleanup and add styled components

--- pages/_document.js
+++ pages/_document.js
@@ -1,23 +1,20 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
-import Script from 'next/script'
+import Document, { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 class MyDocument extends Document {
-
   render() {
     return (
       <Html>
         <Head>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r121/three.min.js"></script>
+          <script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r121/three.min.js"></script>
         </Head>
         <body>
-        
-          
           <Main />
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
 
-export default MyDocument
+export default MyDocument;
--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,25 @@
-import { useState, useEffect, useRef } from 'react'
-import Script from 'next/script'
-import BIRDS from "vanta/dist/vanta.birds.min"
+import { useState, useEffect, useRef } from "react";
+import BIRDS from "vanta/dist/vanta.birds.min";
 
 export default function Home() {
-  
-  const [vantaEffect, setVantaEffect] = useState(0)
-  const myRef = useRef(null)
+  const [vantaEffect, setVantaEffect] = useState(0);
+  const myRef = useRef(null);
   useEffect(() => {
     if (!vantaEffect) {
-      setVantaEffect(BIRDS({
-        el: myRef.current
-      }))
+      setVantaEffect(
+        BIRDS({
+          el: myRef.current,
+        })
+      );
     }
     return () => {
-      if (vantaEffect) vantaEffect.destroy()
-    }
-  }, [vantaEffect])
-  
+      if (vantaEffect) vantaEffect.destroy();
+    };
+  }, [vantaEffect]);
+
   return (
-   <>
-   {/* <Script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r121/three.min.js"></Script> */}
-
-
-
-   <div ref={myRef} style={{height: "100vh", width: "100vw"}}>
-    
-   </div>
-   </>
-  )
+    <>
+      <div ref={myRef} style={{ height: "100vh", width: "100vw" }}></div>
+    </>
+  );
 }
